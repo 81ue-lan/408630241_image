d57de66a39a2c2491a48d34e776393f8f198b953
lan <kitty90508s@gmail.com>
2022-11-14 10:32:53 +0800

    add pic document

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,6 @@ class _HomePageState extends State<HomePage> {
     "https://picsum.photos/id/256/200/300",
     "https://picsum.photos/id/257/200/300",
     "https://picsum.photos/id/258/200/300",
-
   ];
 
   @override
